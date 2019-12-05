11d744705bb9c52f56b7ffa55a4167a7cc0cdd1d
chimuguay <171255@ids.upchiapas.edu.mx>
2019-12-04 22:19:37 -0600

    Configuracion

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -14,7 +14,7 @@ export class ApiService {
 	public id;
 
 	constructor(public _http: HttpClient) {
-		this.baseUrl = "http://ec2-54-167-7-4.compute-1.amazonaws.com/api/v1/";
+		this.baseUrl = "http://ec2-100-24-59-152.compute-1.amazonaws.com/api/v1/";
 	}
 
 
